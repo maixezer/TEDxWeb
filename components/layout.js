91b16d23ac0ac8f1b2c538232a85d881913f97c6
Parinthorn Panya <maixezer@gmail.com>
2017-08-25 02:28:51 +0700

    Update website

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ export default ({ children, title = 'TEDxCharoenkrung' }) => (
       <title>{title}</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initiali-scale=1.0, width=device-width' />
-      <link rel="stylesheet" type="text/css" href="/static/fonts.css" />
+      <link rel="stylesheet" type="text/css" href="static/fonts.css" />
     </Head>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     <header id="tedx_header">
@@ -18,7 +18,7 @@ export default ({ children, title = 'TEDxCharoenkrung' }) => (
     <main id="tedx_main_container">
       {children}
     </main>
-    <script src="/static/js/jquery-3.2.1.slim.min.js" />
-    <script src="/static/js/bootstrap.min.js" />
+    <script src="static/js/jquery-3.2.1.slim.min.js" />
+    <script src="static/js/bootstrap.min.js" />
   </div>
 )
