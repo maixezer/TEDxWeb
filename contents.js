e5bb8b7262a41eb04f39b9d3bbbabdea2cb6ea3a
Parinthorn Panya <maixezer@gmail.com>
2017-09-10 02:37:52 +0700

    Use uuidV4 from contents

--- contents.js
+++ contents.js
@@ -1,28 +1,30 @@
+import uuidv4 from 'uuid/v4'
+
 export default (path) => {
   return [
     {
-      uid: '0665f56d-3a09-4eb7-b187-554acf92fa08',
+      uid: uuidv4(),
       imageUrl: `static/images/${path}/home_bg.jpg`,
       topic: 'Charoenkrung Road',
       desc: 'กรุงเจริญ = เจริญกรุง',
       posted_date: new Date(2017, 8, 20).toISOString()
     },
     {
-      uid: '68ffa3c1-9e2a-44ab-b545-8dbb39bf680c',
+      uid: uuidv4(),
       imageUrl: 'static/images/contents/bg_01.jpg',
       topic: 'Lalalala',
       desc: 'ลาลาลาลา',
       posted_date: new Date(2017, 8, 21).toISOString()
     },
     {
-      uid: '187f08ba-6c44-42a4-8dd8-b356ed3a3c33',
+      uid: uuidv4(),
       imageUrl: 'static/images/contents/bg_02.jpg',
       topic: 'Why Charoenkrung?',
       desc: 'ทำไมต้องเจริญกรุง',
       posted_date: new Date(2017, 8, 22).toISOString()
     },
     {
-      uid: '804b24d3-9a1a-4192-8126-bd1d45998a90',
+      uid: uuidv4(),
       imageUrl: 'static/images/contents/bg_03.jpg',
       topic: 'Everything has its first time...',
       desc: 'ครั้งแรกของ TED',
