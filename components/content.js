5b6e5e0d2a94a6ee4047953979f704c3dd76e371
Parinthorn Panya <maixezer@gmail.com>
2017-09-10 03:22:33 +0700

    Scss for content in blog page

--- components/content.js
+++ components/content.js
@@ -5,20 +5,29 @@ const Content = ({ content, isMobile, enabledHover, fromBlog = false }) => (
   <Link href={{ pathname: '/blog', query: { uid: content.uid } }}>
     <div className={`${enabledHover ? 'content has-hover' : 'content'}`}
       style={{
-        backgroundImage: `url(${content.imageUrl})`, height: '50%'
-        , width: isMobile || fromBlog ? '100%' : '50%'
-        , display: isMobile || fromBlog ? 'block' : 'inline-block'
+        backgroundImage: `url(${content.imageUrl})`,
+        height: fromBlog ? (isMobile ? '50%' : '800px') : '50%',
+        width: isMobile || fromBlog ? '100%' : '50%',
+        display: isMobile || fromBlog ? 'block' : 'inline-block'
       }}>
       <div className="content_container">
         <div className="content_topic_container">
-          <span className="separator"></span>
+          {fromBlog && <span className="separator top"></span>}
           <h1 className="text-center content_topic">{content.topic}</h1>
           <h1 className="text-center content_desc">{content.desc}</h1>
-          <span className="separator"></span>
+          {fromBlog && <span className="separator bottom"></span>}
+          {fromBlog &&
+            <span className="text-center topic_date">
+              Posted date: {moment(content.posted_date).tz('Asia/Bangkok').format('LL')}
+            </span>
+          }
         </div>
-        <div className="text-center content_posted_date">
-          Posted date: {moment(content.posted_date).tz('Asia/Bangkok').format('LL')}
-        </div>
+        {!fromBlog &&
+          <div className="text-center content_posted_date">
+            Posted date: {moment(content.posted_date).tz('Asia/Bangkok').format('LL')}
+          </div>
+        }
+
       </div>
     </div>
   </Link>
