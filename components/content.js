6b6932633136272e2dd7b8a48671a22dd7607b1d
Parinthorn Panya <maixezer@gmail.com>
2017-09-10 18:39:15 +0700

    Fix zomming image

--- components/content.js
+++ components/content.js
@@ -3,33 +3,44 @@ import Link from 'next/link'
 
 const Content = ({ content, isMobile, enabledHover, fromBlog = false }) => (
   <Link href={{ pathname: '/blog', query: { uid: content.uid } }}>
-    <div className={`${enabledHover ? 'content has-hover' : 'content'}`}
+    <div className={fromBlog ? 'content' : 'content has-hover'}
       style={{
-        backgroundImage: `url(${content.image.url})`,
-        height: fromBlog ? (isMobile ? '30%' : '800px') : '50%',
+        height: fromBlog ? (isMobile ? '40%' : '800px') : '50%',
         width: isMobile || fromBlog ? '100%' : '50%',
         display: isMobile || fromBlog ? 'block' : 'inline-block'
       }}>
       {fromBlog ?
         (
-          <div className="image_content_container">
-            <div className="content_topic_container">
-              <span className="separator top"></span>
-              <h1 className="text-center content_topic">{content.image.topic}</h1>
-              <h1 className="text-center content_desc">{content.image.desc}</h1>
-              <span className="separator bottom"></span>
-              <span className="text-center topic_date">
-                Posted date: {moment(content.postedDate).tz('Asia/Bangkok').format('LL')}
-              </span>
+          <div style={{ position: 'relative' }}>
+            <div className="image_content_container" style={{
+              backgroundImage: `url(${content.image.url})`
+            }}>
+            </div>
+            <div className="image_content_topic_container">
+              <div className="image_content">
+                <span className="separator top"></span>
+                <h1 className="text-center content_topic">{content.image.topic}</h1>
+                <h1 className="text-center content_desc">{content.image.desc}</h1>
+                <span className="separator bottom"></span>
+                <h2 className="text-center topic_date">
+                  Posted date: {moment(content.postedDate).tz('Asia/Bangkok').format('LL')}
+                </h2>
+              </div>
             </div>
           </div>
         )
         :
         (
-          <div className="image_content_container">
-            <div className="content_topic_container">
-              <h1 className="text-center content_topic">{content.image.topic}</h1>
-              <h1 className="text-center content_desc">{content.image.desc}</h1>
+          <div style={{ position: 'relative' }}>
+            <div className="image_content_container" style={{
+              backgroundImage: `url(${content.image.url})`
+            }}>
+            </div>
+            <div className="image_content_topic_container">
+              <div className="image_content">
+                <h1 className="text-center content_topic">{content.image.topic}</h1>
+                <h1 className="text-center content_desc">{content.image.desc}</h1>
+              </div>
             </div>
             <div className="text-center content_posted_date">
               Posted date: {moment(content.postedDate).tz('Asia/Bangkok').format('LL')}
