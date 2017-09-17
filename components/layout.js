9ea77de96d49b60b61b6b4f225868bdb6e25f455
Parinthorn Panya <maixezer@gmail.com>
2017-09-17 14:55:11 +0700

    Add GA for website per page

--- components/layout.js
+++ components/layout.js
@@ -1,32 +1,54 @@
 import Head from 'next/head'
-import React from 'react'
+import { Component } from 'react'
 import Navbar from './navbar'
+import { initGA, logPageView } from '../utils'
 
 import stylesheet from 'styles/application.scss'
 
-const Layout = ({ children, isMobile, navbarColor, currentPage, styles = { height: 'inherit', width: 'inherit', bgColor: '#fff', headerColor: 'transparent' } }) => (
-  <div id="tedx_body">
-    <Head>
-      <title>TEDxCharoenkrung</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initiali-scale=1.0, width=device-width' />
-    </Head>
-    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-    <header id="tedx_header" style={{ 'backgroundColor': styles.headerColor }}>
-      <Navbar currentPage={currentPage} isMobile={isMobile} navbarColor={navbarColor} />
-    </header>
-    <main id="tedx_main_container"
-      style={{
-        height: styles.height,
-        width: styles.width,
-        backgroundColor: styles.bgColor,
-        display: 'table'
-      }}>
-      {children}
-    </main>
-    <script src="../static/js/jquery-3.2.1.slim.min.js" />
-    <script src="../static/js/bootstrap.min.js" />
-  </div>
-)
+export default class Layout extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-export default Layout
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
+
+  render() {
+    const { children, isMobile, navbarColor, currentPage, styles = { height: 'inherit', width: 'inherit', bgColor: '#fff', headerColor: 'transparent' } } = this.props
+    return (
+      <div id="tedx_body">
+        <Head>
+          <title>TEDxCharoenkrung</title>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='initiali-scale=1.0, width=device-width' />
+        </Head>
+        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+        <header id="tedx_header" style={{ 'backgroundColor': styles.headerColor }}>
+          <Navbar currentPage={currentPage} isMobile={isMobile} navbarColor={navbarColor} />
+        </header>
+        <main id="tedx_main_container"
+          style={{
+            height: styles.height,
+            width: styles.width,
+            backgroundColor: styles.bgColor,
+            display: 'table'
+          }}>
+          {children}
+        </main>
+        <script src="../static/js/jquery-3.2.1.slim.min.js" />
+        <script src="../static/js/bootstrap.min.js" />
+      </div>
+    )
+  }
+}
+
+// const Layout = ({ children, isMobile, navbarColor, currentPage, styles = { height: 'inherit', width: 'inherit', bgColor: '#fff', headerColor: 'transparent' } }) => (
+
+// )
+
+// export default Layout
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+import ReactGA from 'react-ga'
+
 export const isElementInView = (element, fullyInView = false) => {
   const isjQueryObjExists = (obj) => (
     $(obj).length !== 0
@@ -20,4 +22,27 @@ export const easeInOutQuad = (t, b, c, d) => {
   if (t < 1) return c / 2 * t * t + b
   t--
   return -c / 2 * (t * (t - 2) - 1) + b
+}
+
+export const initGA = () => {
+  console.log('GA init')
+  ReactGA.initialize('UA-106544318-1')
+}
+
+export const logPageView = () => {
+  console.log(`Logging pageview for ${window.location.pathname}`)
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
+
+export const logEvent = (category = '', action = '') => {
+  if (category && action) {
+    ReactGA.event({ category, action })
+  }
+}
+
+export const logException = (description = '', fatal = false) => {
+  if (description) {
+    ReactGA.exception({ description, fatal })
+  }
 }
